0897fb07390a1cdebe9d3dacb4d8014c62489f8c
Nenotriple <70049990+Nenotriple@users.noreply.github.com>
2023-02-21 11:58:59 -0800

    Update img-txt_viewer.py
    Added image counter, various UI tweaks.

--- img-txt_viewer.py
+++ img-txt_viewer.py
@@ -7,7 +7,6 @@ class ImageTextViewer:
     def __init__(self, master):
         self.master = master
         master.title("Image Text Viewer - ctrl+s Saves - alt+arrow Next/Previous Pair")
-        master.geometry("512x1000")
         master.bind("<Control-s>", lambda event: self.save_text_file())
         master.bind("<Alt-Left>", lambda event: self.prev_pair())
         master.bind("<Alt-Right>", lambda event: self.next_pair())
@@ -18,7 +17,7 @@ class ImageTextViewer:
         self.text_files = []
 
         self.label_image = Label(master)
-        self.text_box = Text(master, height=30, width=100, wrap=WORD)
+        self.text_box = Text(master, height=20, width=50, wrap=WORD)
         self.next_button = Button(master, text="Next", command=self.next_pair)
         self.prev_button = Button(master, text="Previous", command=self.prev_pair)
         self.directory_button = Button(master, text="Choose Directory", command=self.choose_directory)
@@ -28,15 +27,17 @@ class ImageTextViewer:
         self.auto_save_var.set(False)
         self.text_modified = False
 
-        self.directory_entry.pack(side=TOP, expand=NO, fill=X)
+        self.directory_entry.pack(side=TOP, fill=X)
         self.directory_button.pack(side=TOP, fill=X)
-        self.label_image.pack(side=TOP, expand=YES)
-        self.next_button.pack(anchor=N, fill=X, pady=3)
-        self.prev_button.pack(anchor=N, fill=X, pady=3)
-        self.save_button.pack(side=TOP, fill=X,)
+        self.label_image.pack(side=LEFT)
+        self.next_button.pack(anchor=N, fill=X, ipadx=120, pady=3)
+        self.prev_button.pack(anchor=N, fill=X, ipadx=100, pady=3)
+        self.save_button.pack(side=TOP, fill=X, ipadx=100, pady=3)
         self.auto_save_checkbutton = Checkbutton(master, text="Auto-save", variable=self.auto_save_var)
-        self.auto_save_checkbutton.pack(side=TOP,)
+        self.auto_save_checkbutton.pack(side=TOP)
 
+        self.image_index_label = Label(self.master, text="Load some images!")
+        self.image_index_label.pack(side=TOP, expand=NO)
 
         self.create_saved_label()
         self.text_box.bind("<Key>", lambda event: self.change_label())
@@ -45,10 +46,9 @@ class ImageTextViewer:
         self.saved_label = Label(self.master, text="No Changes")
         self.saved_label.pack(side=TOP, expand=NO)
         self.text_box.bind("<Key>", lambda event: self.text_modified())
-        self.text_box.pack(side=BOTTOM, expand=YES, fill=BOTH)
 
     def change_label(self):
-        self.saved_label.config(text="Text Not Saved", fg="red")
+        self.saved_label.config(text="Changes Not Saved", bg="red", fg="white")
 
     def choose_directory(self):
         self.image_dir.set(askdirectory())
@@ -64,6 +64,7 @@ class ImageTextViewer:
                 text_filename = os.path.splitext(filename)[0] + ".txt"
                 self.text_files.append(os.path.join(self.image_dir.get(), text_filename))
         self.show_pair()
+        self.display_image_count()
 
     def show_pair(self):
         if self.image_files:
@@ -87,8 +88,17 @@ class ImageTextViewer:
                 self.text_box.insert(END, f.read())
                 self.text_modified = False
             if not self.text_modified:
-                self.saved_label.config(text="No Changes", fg="black")
-
+                self.saved_label.config(text="No Changes", bg="white", fg="black")
+            self.display_image_index()
+    
+    def display_image_index(self):
+        if hasattr(self, 'image_index_label'):
+            self.image_index_label.config(text=f"Image {self.current_index + 1}/{len(self.image_files)}")
+        else:
+            self.image_index_label = Label(self.master, text=f"Image {self.current_index + 1}/{len(self.image_files)}")
+            self.image_index_label.pack(side=TOP, expand=YES)
+        self.text_box.pack(side=BOTTOM, expand=YES, fill=BOTH)
+    
     def next_pair(self):
         if self.current_index < len(self.image_files) - 1:
             if self.auto_save_var.get():
@@ -106,7 +116,7 @@ class ImageTextViewer:
             self.show_pair()
             if not self.text_modified:
                 self.saved_label.config(text="No Changes", fg="black")
-
+    
     def save_text_file(self):
         if self.text_files:
             text_file = self.text_files[self.current_index]
@@ -120,10 +130,11 @@ class ImageTextViewer:
             with open(text_file, "r") as f:
                 file_content = f.read()
                 text_content = self.text_box.get("1.0", END)
-                if file_content == text_content:
-                    self.saved_label.config(text="saved")
+                self.text_modified = file_content != text_content
+                if self.text_modified:
+                    self.saved_label.config(text="Changes Not Saved", fg="red")
                 else:
-                    self.saved_label.config(text="unsaved", fg="red")
+                    self.saved_label.config(text="No Changes", fg="black")
 
     def text_modified(self):
         self.saved_label.config(text="Unsaved", fg="red")
@@ -133,7 +144,7 @@ class ImageTextViewer:
             text_file = self.text_files[self.current_index]
             with open(text_file, "w") as f:
                 f.write(self.text_box.get("1.0", END))
-            self.saved_label.config(text="Text Saved", fg="green")
+            self.saved_label.config(text="Saved", bg="green", fg="white")
 
 root = Tk()
 app = ImageTextViewer(root)
