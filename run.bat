4017f9107490a68a02d7f702cddb455662f53523
Nenotriple <70049990+Nenotriple@users.noreply.github.com>
2023-04-04 15:16:34 -0700

    No longer needed. Just open with .pyw extension

--- run.bat
+++
@@ -1,6 +0,0 @@
-@echo OFF
-if not DEFINED IS_MINIMIZED set IS_MINIMIZED=1 && start "" /min "%~dpnx0" %* && exit
-python img-txt_viewer.py
-exit
-
-::This bat file is only used as a way of hiding the CMD prompt that opens with the img-txt_viewer ui window.
