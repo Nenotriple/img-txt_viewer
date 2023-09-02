6193b53bd546211d6945f377e6d657360b91a1e9
Nenotriple <70049990+Nenotriple@users.noreply.github.com>
2023-09-01 23:29:31 -0700

    New Feature: Autocomplete using danbooru tags
    This is a preliminary implementation, several things should still be addressed eventually.
    
    -Cursor position is not fixed while holding tab: This can lead to situations where the cursor may overlap with another word and upon inserted the suggestion the other word would be replaced.
    
    -Some strange occurances of autocomplete suggestions not populating the list as expected.

--- img-txt_viewer.py
+++ img-txt_viewer.py
@@ -2,23 +2,56 @@ from tkinter import *
 from tkinter.filedialog import askdirectory
 from tkinter import messagebox
 from PIL import ImageTk, Image
+from difflib import get_close_matches
+import csv
 import os
 import re
+
+class Autocomplete:
+    def __init__(self, data_file, autocomplete_delay=75, max_suggestions=3):
+        self.data = self.load_data(data_file)
+        self.autocomplete_delay = autocomplete_delay
+        self.max_suggestions = max_suggestions
+
+    def load_data(self, data_file):
+        data = {}
+        with open(data_file, newline='', encoding='utf-8') as csvfile:
+            reader = csv.reader(csvfile)
+            for row in reader:
+                true_name = row[0]
+                similar_names = row[3].split(',')
+                data[true_name] = similar_names
+        return data
+
+    def autocomplete(self, text):
+        suggestions = []
+        text_without_spaces = text.replace(" ", "_")
+        for true_name, similar_names in self.data.items():
+            if true_name.startswith(text_without_spaces):
+                suggestions.append((true_name, similar_names))
+            else:
+                for sim_name in similar_names:
+                    if sim_name.startswith(text_without_spaces):
+                        suggestions.append((true_name, similar_names))
+                        break
+        return suggestions[:self.max_suggestions]
 
 class ImageTextViewer:
     def __init__(self, master):
         self.master = master
-        master.title("v1.5 - img-txt_Viewer - Use CTRL + S to save - Use ALT + Left/Right keys to move between img/txt pairs")
+        master.title("v1.6 - img-txt_Viewer: (CTRL + S Quicksaves) (ALT + Left/Right Moves between img/txt pairs) (TAB inserts selected suggestion, TAB+Left/Right Selects suggestion)")
         master.bind("<Control-s>", lambda event: self.save_text_file())
         master.bind("<Alt-Left>", lambda event: self.prev_pair())
         master.bind("<Alt-Right>", lambda event: self.next_pair())
-        self.master.minsize(750, 600)
+        self.master.minsize(1025, 600)
         self.image_dir = StringVar()
         self.current_index = 0
         self.image_files = []
         self.text_files = []
+        self.suggestions = []
         self.label_image = Label(master)
         self.text_box = Text(master, height=20, width=50, wrap=WORD, undo=TRUE)
+        self.suggestion_label = Label(master, text="")
         self.next_button = Button(master, text="Next", command=self.next_pair)
         self.prev_button = Button(master, text="Previous", command=self.prev_pair)
         self.directory_button = Button(master, text="Choose Directory", command=self.choose_directory)
@@ -27,7 +60,7 @@ class ImageTextViewer:
         self.auto_save_var = BooleanVar()
         self.auto_save_var.set(False)
         self.text_modified = False
-        self.directory_entry.pack(side=TOP, fill=X)
+        self.directory_entry.pack(side=TOP, fill=X)               
         self.directory_button.pack(side=TOP, fill=X)
         self.label_image.pack(side=LEFT)
         self.next_button.pack(anchor=N, fill=X, ipadx=120, pady=3)
@@ -38,13 +71,75 @@ class ImageTextViewer:
         self.create_saved_label()
         self.image_index_label = Label(self.master, text="Load a directory with image and text pairs.")
         self.image_index_label.pack(anchor=N)
+        self.suggestion_label.pack(side=TOP, fill=X)
         self.text_box.bind("<Key>", lambda event: self.change_label())
         self.text_box.tag_configure("highlight", background="lightblue")
         self.text_box.bind("<ButtonRelease-1>", lambda event: self.highlight_duplicates(event, mouse=True))
         self.text_box.bind("<KeyRelease>", lambda event: self.highlight_duplicates(event, mouse=False))
         self.text_box.bind("<Left>", lambda event: self.remove_highlight())
-        self.text_box.bind("<Right>", lambda event: self.remove_highlight())       
+        self.text_box.bind("<Right>", lambda event: self.remove_highlight())
+        self.autocomplete = Autocomplete("danbooru.csv")
+        self.text_box.bind("<KeyRelease>", self.update_suggestions) 
+        self.selected_suggestion_index = 0
 
+    def update_suggestions(self, event):
+        if event.keysym == "Tab":
+            if self.selected_suggestion_index < len(self.suggestions):
+                completed_suggestion = self.suggestions[self.selected_suggestion_index]
+                self.insert_completed_suggestion(completed_suggestion)
+            self.suggestion_label.config(text="")
+            self.suggestions = []
+        elif event.keysym in ("Left", "Right"):
+            if self.suggestions:
+                if event.keysym == "Left":
+                    self.selected_suggestion_index = (self.selected_suggestion_index - 1) % len(self.suggestions)
+                else:
+                    self.selected_suggestion_index = (self.selected_suggestion_index + 1) % len(self.suggestions)
+                self.highlight_selected_suggestion()
+        elif event.keysym in ("Up", "Down"):
+            self.suggestion_label.config(text="")
+            self.suggestions = []
+        elif event.char == ",":
+            self.suggestion_label.config(text="")
+            self.suggestions = []
+        else:
+            if event.char and event.char.isalnum():
+                text = self.text_box.get("1.0", "insert")
+                elements = [element.strip() for element in text.split(',')]
+                current_word = elements[-1]
+                current_word = current_word.strip()
+                if current_word:
+                    self.suggestions = [suggestion[0].replace("_", " ") for suggestion in self.autocomplete.autocomplete(current_word)]
+                    if self.suggestions:
+                        suggestion_text = "Suggestions: " + ", ".join(self.suggestions)
+                        self.suggestion_label.config(text=suggestion_text)
+                        self.selected_suggestion_index = 0
+                        self.highlight_selected_suggestion()
+                    else:
+                        self.suggestion_label.config(text="")
+                else:
+                    self.suggestion_label.config(text="")
+
+    def insert_completed_suggestion(self, completed_suggestion):
+        text = self.text_box.get("1.0", "insert")
+        elements = [element.strip() for element in text.split(',')]
+        current_word = elements[-1]
+        current_word = current_word.strip()
+        updated_text = text.rsplit(',', 1)[0] + ', ' + completed_suggestion + ', '
+        self.text_box.delete("1.0", "end")
+        self.text_box.insert("1.0", updated_text)
+    
+    def highlight_selected_suggestion(self):
+        if self.suggestions:
+            suggestion_text = "Suggestions: "
+            for i, s in enumerate(self.suggestions):
+                if i == self.selected_suggestion_index:
+                    label_text = f"[{s}]"
+                else:
+                    label_text = s
+                suggestion_text += label_text + ", "
+            self.suggestion_label.config(text=suggestion_text[:-2])
+    
     def create_saved_label(self):
         self.saved_label = Label(self.master, text="No Changes")
         self.saved_label.pack(anchor=N)
@@ -122,7 +217,8 @@ class ImageTextViewer:
             if not self.text_modified:
                 self.saved_label.config(text="No Changes", bg="white", fg="black")
             self.text_box.bind("<ButtonRelease-1>", self.highlight_duplicates)
-            self.text_box.bind("<Button-1>", lambda event: self.remove_highlight())    
+            self.text_box.bind("<Button-1>", lambda event: self.remove_highlight())
+            self.text_box.bind("<Tab>", self.disable_tab)   
             self.display_image_index()
 
     def highlight_duplicates(self, event, mouse=True):
@@ -190,6 +286,9 @@ class ImageTextViewer:
             with open(text_file, "w") as f:
                 f.write(self.text_box.get("1.0", END).strip())
             self.saved_label.config(text="Saved", bg="green", fg="white")
+
+    def disable_tab(self, event):
+        return "break"            
 
 root = Tk()
 app = ImageTextViewer(root)
