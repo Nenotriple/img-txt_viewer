7c47d7b4d0ee8f88a8f49b3fdac0f98c08994c6f
Nenotriple <70049990+Nenotriple@users.noreply.github.com>
2024-11-13 19:25:20 -0800

    Update Start.bat
    Add option to install requirements.txt if it exists.

--- Start.bat
+++ Start.bat
@@ -1,5 +1,5 @@
 @echo off
-setlocal
+setlocal enabledelayedexpansion
 
 
 REM Set up a Python virtual environment and run a specified Python script.
@@ -13,17 +13,23 @@ set "SCRIPT_DIR=%~dp0"
 
 REM Navigate to the script directory
 cd /d "%SCRIPT_DIR%" || (
-    echo Set current directory... FAIL
+    echo Failed to set current directory.
     pause
     exit /b 1
 )
 echo Set current directory... OK
 
 
+REM Check if Python is installed
+python --version >nul 2>&1 || (
+    echo Python is not installed or not found in PATH.
+    pause
+    exit /b 1
+)
+
+
 REM Check if the virtual environment already exists
-if exist "%VENV_DIR%" (
-    echo Virtual environment already exists: "%SCRIPT_DIR%\%VENV_DIR%"
-) else (
+if not exist "%VENV_DIR%\Scripts\activate.bat" (
     echo Creating a Python virtual environment: "%SCRIPT_DIR%\%VENV_DIR%"
     python -m venv "%VENV_DIR%" || (
         echo An error occurred while creating the virtual environment.
@@ -31,16 +37,38 @@ if exist "%VENV_DIR%" (
         exit /b 1
     )
     echo Virtual environment created.
+) else (
+    echo Virtual environment already exists: "%SCRIPT_DIR%\%VENV_DIR%"
 )
 
 
 REM Activate the virtual environment
-call "%SCRIPT_DIR%\%VENV_DIR%\Scripts\activate.bat" || (
+call "%VENV_DIR%\Scripts\activate.bat" || (
     echo Activating the virtual environment... FAIL
     pause
     exit /b 1
 )
 echo Virtual environment activated.
+
+
+REM Upgrade pip
+python -m pip install --upgrade pip
+
+
+REM Install requirements if requirements.txt exists
+if exist "requirements.txt" (
+    set /p INSTALL_REQ="requirements.txt found. Do you want to install the requirements? (Y/N): "
+    if /i "!INSTALL_REQ!"=="Y" (
+        echo Installing requirements...
+        pip install -r requirements.txt || (
+            echo Failed to install requirements.
+            pause
+            exit /b 1
+        )
+    ) else (
+        echo Skipping requirements installation.
+    )
+)
 
 
 REM Launch the user-defined Python script
