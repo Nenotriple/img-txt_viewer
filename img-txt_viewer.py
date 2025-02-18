3a4b401f1de278fdb25c259e409956954ea22b02
Nenotriple <70049990+Nenotriple@users.noreply.github.com>
2025-02-17 21:48:26 -0800

    Update img-txt_viewer.py
    Import and setup new tools.
    
    Setup 'create' functions for new tools.

--- img-txt_viewer.py
+++ img-txt_viewer.py
@@ -58,7 +58,9 @@ from main.scripts import (
     batch_tag_edit,
     find_dupe_file,
     text_controller,
+    batch_convert,
     upscale_image,
+    batch_rename,
     resize_image,
     image_grid,
     edit_panel,
@@ -101,6 +103,8 @@ class ImgTxtViewer:
         self.settings_manager = settings_manager.SettingsManager(self, self.root)
         self.stat_calculator = calculate_file_stats.CalculateFileStats(self, self.root)
         self.batch_resize_images = batch_resize_images.BatchResizeImages()
+        self.batch_rename = batch_rename.BatchRename()
+        self.batch_convert = batch_convert.BatchConvert()
         self.edit_panel = edit_panel.EditPanel(self, self.root)
         self.batch_tag_edit = batch_tag_edit.BatchTagEdit()
         self.find_dupe_file = find_dupe_file.FindDupeFile()
@@ -1352,11 +1356,27 @@ class ImgTxtViewer:
 
 
     def create_batch_rename_ui(self, show=False):
-        pass
+        parent = self
+        root = self.root
+        path = self.image_dir.get()
+        if self.batch_rename.parent is None:
+            self.batch_rename.setup_window(parent, root, path)
+        if show:
+            self.main_notebook.select(self.batch_rename_tab)
+            if self.batch_rename.working_dir != self.image_dir.get():
+                self.batch_rename.select_folder(self.image_dir.get())
 
 
     def create_batch_convert_ui(self, show=False):
-        pass
+        parent = self
+        root = self.root
+        path = self.image_dir.get()
+        if self.batch_convert.parent is None:
+            self.batch_convert.setup_window(parent, root, path)
+        if show:
+            self.main_notebook.select(self.batch_convert_tab)
+            if self.batch_convert.working_dir != self.image_dir.get():
+                self.batch_convert.select_folder(self.image_dir.get())
 
 
     def create_find_dupe_file_ui(self, show=False):
